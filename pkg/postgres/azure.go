3708d5f42980085c0988e9e31c277a494a724f14
Pieter Callewaert <pieter@nephroflow.com>
2019-12-16 10:01:45 +0100

    fix: changing owner transfership failed

--- pkg/postgres/azure.go
+++ pkg/postgres/azure.go
@@ -3,6 +3,8 @@ package postgres
 import (
 	"fmt"
 	"strings"
+
+	"github.com/lib/pq"
 )
 
 type azurepg struct {
@@ -24,9 +26,37 @@ func (azpg *azurepg) GetLoginForRole(role string) string {
 	return role
 }
 
+func (azpg *azurepg) GetRoleForLogin(login string) string {
+	splitUser := strings.Split(azpg.user, "@")
+	if len(splitUser) > 1 {
+		return splitUser[0]
+	}
+	return login
+}
+
 func (azpg *azurepg) Connect() error {
 	// Default database for azure is postgres
 	// https://docs.microsoft.com/en-us/azure/postgresql/concepts-servers#managing-your-server
 	azpg.db = GetConnection(azpg.user, azpg.pass, azpg.host, "postgres", azpg.args, azpg.log)
+	return nil
+}
+
+func (azpg *azurepg) CreateDB(dbname, role string) error {
+	_, err := azpg.db.Exec(fmt.Sprintf(CREATE_DB, dbname))
+	if err != nil {
+		// eat DUPLICATE DATABASE ERROR
+		if err.(*pq.Error).Code != "42P04" {
+			return err
+		}
+	}
+	// Have to add the master role to the group role before we can transfer the database owner
+	err = azpg.GrantRole(role, azpg.GetRoleForLogin(azpg.user))
+	if err != nil {
+		return err
+	}
+	_, err = azpg.db.Exec(fmt.Sprintf(ALTER_DB_OWNER, dbname, role))
+	if err != nil {
+		return err
+	}
 	return nil
 }
