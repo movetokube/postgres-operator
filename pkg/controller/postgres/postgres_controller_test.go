f1e98138f7dd2d285dc319340615370462b0fdf1
Nishant virmani <nvrmanni720@gmail.com>
2021-03-13 13:47:40 +0530

    changes in test cases call

--- pkg/controller/postgres/postgres_controller_test.go
+++ pkg/controller/postgres/postgres_controller_test.go
@@ -276,6 +276,8 @@ var _ = Describe("ReconcilePostgres", func() {
 				pg.EXPECT().CreateGroupRole(gomock.Any()).Return(nil).AnyTimes()
 				// CreateDB call
 				pg.EXPECT().CreateDB(name, expectedName).Return(nil)
+				// ModifyTables call
+				pg.EXPECT().ModifyTables(name, expectedName, gomock.Any()).Return(nil)
 				// Call Reconcile
 				rp.Reconcile(req)
 			})
@@ -306,6 +308,8 @@ var _ = Describe("ReconcilePostgres", func() {
 				pg.EXPECT().CreateGroupRole(gomock.Any()).Return(nil).AnyTimes()
 				// CreateDB call
 				pg.EXPECT().CreateDB(name, expectedName).Return(nil)
+				// ModifyTables call
+				pg.EXPECT().ModifyTables(name, expectedName, gomock.Any()).Return(nil)
 				// Call Reconcile
 				rp.Reconcile(req)
 			})
@@ -327,6 +331,8 @@ var _ = Describe("ReconcilePostgres", func() {
 				// Expected function calls
 				pg.EXPECT().CreateGroupRole(gomock.Any()).Return(nil).Times(3)
 				pg.EXPECT().CreateDB(name, gomock.Any()).Return(nil)
+				// ModifyTables call
+				pg.EXPECT().ModifyTables(name, gomock.Any(), gomock.Any()).Return(nil)
 			})
 
 			It("should update status", func() {
@@ -375,6 +381,8 @@ var _ = Describe("ReconcilePostgres", func() {
 				// Expected function calls
 				pg.EXPECT().CreateGroupRole(gomock.Any()).Return(nil).Times(1)
 				pg.EXPECT().CreateDB(name, gomock.Any()).Return(fmt.Errorf("Could not create database"))
+				// ModifyTables call
+				pg.EXPECT().ModifyTables(name, gomock.Any(), gomock.Any()).Return(fmt.Errorf("Could not create function"))
 			})
 
 			It("should not update status", func() {
