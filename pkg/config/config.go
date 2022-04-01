7ad54f8e97913e57a2eb6c8a92b7290edbe2db64
Enderson Maia <endersonmaia@gmail.com>
2022-04-01 07:54:12 -0300

    Escape special characters for POSTGRES_URL (#79)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
-	"github.com/movetokube/postgres-operator/pkg/utils"
+	"net/url"
 	"sync"
+
+	"github.com/movetokube/postgres-operator/pkg/utils"
 )
 
 type cfg struct {
@@ -21,9 +23,9 @@ func Get() *cfg {
 	doOnce.Do(func() {
 		config = &cfg{}
 		config.PostgresHost = utils.MustGetEnv("POSTGRES_HOST")
-		config.PostgresUser = utils.MustGetEnv("POSTGRES_USER")
-		config.PostgresPass = utils.MustGetEnv("POSTGRES_PASS")
-		config.PostgresUriArgs = utils.MustGetEnv("POSTGRES_URI_ARGS")
+		config.PostgresUser = url.PathEscape(utils.MustGetEnv("POSTGRES_USER"))
+		config.PostgresPass = url.PathEscape(utils.MustGetEnv("POSTGRES_PASS"))
+		config.PostgresUriArgs = url.QueryEscape(utils.MustGetEnv("POSTGRES_URI_ARGS"))
 		config.PostgresDefaultDb = utils.GetEnv("POSTGRES_DEFAULT_DATABASE")
 		config.CloudProvider = utils.GetEnv("POSTGRES_CLOUD_PROVIDER")
 	})
