f9f6a1a93de7ac6f55cbe85af4935017622f3ac9
Alan Clucas <alan@clucas.org>
2022-10-29 10:22:12 +0000

    Rename ConnectionString->Dotnet

--- pkg/controller/postgresuser/postgresuser_controller.go
+++ pkg/controller/postgresuser/postgresuser_controller.go
@@ -271,7 +271,7 @@ func (r *ReconcilePostgresUser) addFinalizer(reqLogger logr.Logger, m *dbv1alpha
 func (r *ReconcilePostgresUser) newSecretForCR(cr *dbv1alpha1.PostgresUser, role, password, login string) *corev1.Secret {
 	pgUserUrl := fmt.Sprintf("postgresql://%s:%s@%s/%s", role, password, r.pgHost, cr.Status.DatabaseName)
 	pgJDBCUrl := fmt.Sprintf("jdbc:postgresql://%s/%s", r.pgHost, cr.Status.DatabaseName)
-	pgConnectionString := fmt.Sprintf("User ID=%s;Password=%s;Host=%s;Port=5432;Database=%s;", role, password, r.pgHost, cr.Status.DatabaseName)
+	pgDotnetUrl := fmt.Sprintf("User ID=%s;Password=%s;Host=%s;Port=5432;Database=%s;", role, password, r.pgHost, cr.Status.DatabaseName)
 	labels := map[string]string{
 		"app": cr.Name,
 	}
@@ -285,14 +285,14 @@ func (r *ReconcilePostgresUser) newSecretForCR(cr *dbv1alpha1.PostgresUser, role
 			Annotations: annotations,
 		},
 		Data: map[string][]byte{
-			"POSTGRES_URL":      []byte(pgUserUrl),
-			"POSTGRES_JDBC_URL": []byte(pgJDBCUrl),
-			"POSTGRES_CONNECTIONSTRING": []byte(pgConnectionString),
-			"HOST":              []byte(r.pgHost),
-			"DATABASE_NAME":     []byte(cr.Status.DatabaseName),
-			"ROLE":              []byte(role),
-			"PASSWORD":          []byte(password),
-			"LOGIN":             []byte(login),
+			"POSTGRES_URL":        []byte(pgUserUrl),
+			"POSTGRES_JDBC_URL":   []byte(pgJDBCUrl),
+			"POSTGRES_DOTNET_URL": []byte(pgDotnetUrl),
+			"HOST":                []byte(r.pgHost),
+			"DATABASE_NAME":       []byte(cr.Status.DatabaseName),
+			"ROLE":                []byte(role),
+			"PASSWORD":            []byte(password),
+			"LOGIN":               []byte(login),
 		},
 	}
 }
