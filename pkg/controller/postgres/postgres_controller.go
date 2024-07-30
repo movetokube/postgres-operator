1fed88a265e460b2c3e6e167605471bda2ebcdf8
Grandmax <maxime.dupre@wttj.co>
2024-07-30 17:36:01 +0200

    try and log

--- pkg/controller/postgres/postgres_controller.go
+++ pkg/controller/postgres/postgres_controller.go
@@ -299,7 +299,9 @@ func (r *ReconcilePostgres) shouldDropDB(cr *dbv1alpha1.Postgres, logger logr.Lo
 
 	for _, user := range users.Items {
 		// There are existing user CRDs bound to this CR, let's not drop it yet
-		if user.Spec.Database == cr.Spec.Database {
+		if user.Spec.Database == cr.Name {
+			logger.Info("Found user for DB")
+			logger.Info(fmt.Sprintf("%v", user.Spec.Database))
 			return false
 		}
 	}
